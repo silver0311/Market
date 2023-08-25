44832d35e503a30eae5a7b3326d399d2372f1da7
jaime castaño <jimmysilver11@outlook.es>
2023-08-25 12:57:54 -0500

    Project initial Markert

---
+++ Market/Maeket.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maeket.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
